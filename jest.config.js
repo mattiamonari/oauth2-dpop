894e681610c0c69cf6212fb3096d91867cab358a
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-08-11 16:25:46 +0800

    build: configure Jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testRegex: "(/tests/.*|\\.(test|spec))\\.(ts|tsx)$",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+};
