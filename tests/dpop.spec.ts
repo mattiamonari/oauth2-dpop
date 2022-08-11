8500a5e27c8cb53673c31fda140e71df6a52b4c6
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-08-11 16:51:31 +0800

    refactor(dpop-test): use createToken instead of hardcoded tokens

--- tests/dpop.spec.ts
+++ tests/dpop.spec.ts
@@ -1,11 +1,31 @@
 import { verifyDPoP } from "../src/dpop";
 
+import { createToken } from "./utils";
+
 describe("DPoP", () => {
   it("should reject DPoPs with invalid type", () => {
     expect(() =>
       verifyDPoP(
         // Token with "typ" header claim: "$pop+jwt"
-        "eyJ0eXAiOiIkcG9wK2p3dCIsImFsZyI6IkVTMjU2IiwiandrIjp7Imt0eSI6IkVDIiwieCI6Imw4dEZyaHgtMzR0VjNoUklDUkRZOXpDa0RscEJoRjQyVVFVZldWQVdCRnMiLCJ5IjoiOVZFNGpmX09rX282NHpiVFRsY3VOSmFqSG10NnY5VERWclUwQ2R2R1JEQSIsImNydiI6IlAtMjU2In19.eyJqdGkiOiItQndDM0VTYzZhY2MybFRjIiwiaHRtIjoiUE9TVCIsImh0dSI6Imh0dHBzOi8vc2VydmVyLmV4YW1wbGUuY29tL3Rva2VuIiwiaWF0IjoxNTYyMjYyNjE2fQ.2-GxA6T8lP4vfrg8v-FdWP0A0zdrj8igiMLvqRMUvwnQg4PtFLbdLXiOSsX0x7NVY-FNyJK70nfbV37xRZT3Lg"
+        createToken([
+          {
+            typ: "$pop+jwt",
+            alg: "ES256",
+            jwk: {
+              kty: "EC",
+              x: "l8tFrhx-34tV3hRICRDY9zCkDlpBhF42UQUfWVAWBFs",
+              y: "9VE4jf_Ok_o64zbTTlcuNJajHmt6v9TDVrU0CdvGRDA",
+              crv: "P-256",
+            },
+          },
+          {
+            jti: "-BwC3ESc6acc2lTc",
+            htm: "POST",
+            htu: "https://server.example.com/token",
+            iat: 1562262616,
+          },
+          Buffer.alloc(0),
+        ])
       )
     ).toThrow();
   });
