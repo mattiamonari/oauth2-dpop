aa153f749a3f8164f818f236f625caadad826b57
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-08-11 17:21:09 +0800

    refactor: introduce separate verifyHeader function

--- src/dpop.ts
+++ src/dpop.ts
@@ -3,31 +3,20 @@ import jose, { JWK } from "node-jose";
 import { parseJWT } from "./jwt";
 import { DPoPError, JWTError } from "./errors";
 
-export async function verifyDPoP(token: unknown) {
-  const [header] = parseJWT(token);
-
-  let parsedHeader: Record<string, unknown>;
-  try {
-    parsedHeader = JSON.parse(
-      jose.util.base64url.decode(header).toString("utf-8")
-    );
-  } catch {
-    throw new JWTError("malformed token");
-  }
-
+async function verifyHeader(header: Record<string, unknown>): Promise<JWK.Key> {
   // See: https://datatracker.ietf.org/doc/html/draft-ietf-oauth-dpop-11#section-4.2
   // typ claim must be dpop+jwt
-  if (!("typ" in parsedHeader) || parsedHeader.typ !== "dpop+jwt") {
+  if (!("typ" in header) || header.typ !== "dpop+jwt") {
     throw new DPoPError("malformed token");
   }
   // alg: a digital signature algorithm identifier such as per RFC7518.
   // MUST NOT be none or an identifier for a symmetric algorithm (MAC).
   if (
-    !("alg" in parsedHeader) ||
-    typeof parsedHeader.alg !== "string" ||
+    !("alg" in header) ||
+    typeof header.alg !== "string" ||
     // See: https://datatracker.ietf.org/doc/html/rfc7518#section-3.1
-    parsedHeader.alg.startsWith("HS") ||
-    parsedHeader.alg === "none"
+    header.alg.startsWith("HS") ||
+    header.alg === "none"
   ) {
     throw new DPoPError("malformed token");
   }
@@ -35,13 +24,13 @@ export async function verifyDPoP(token: unknown) {
   // RFC7517 format, as defined in Section 4.1.3 of RFC7515.
   // MUST NOT contain a private key.
   if (
-    !("jwk" in parsedHeader) ||
-    typeof parsedHeader.jwk !== "object" ||
-    parsedHeader.jwk === null
+    !("jwk" in header) ||
+    typeof header.jwk !== "object" ||
+    header.jwk === null
   ) {
     throw new DPoPError("malformed token");
   }
-  const { jwk } = parsedHeader;
+  const { jwk } = header;
 
   let key;
   try {
@@ -49,4 +38,21 @@ export async function verifyDPoP(token: unknown) {
   } catch (e) {
     throw new DPoPError("malformed token");
   }
+
+  return key;
+}
+
+export async function verifyDPoP(token: unknown) {
+  const [rawHeader] = parseJWT(token);
+
+  let header: Record<string, unknown>;
+  try {
+    header = JSON.parse(
+      jose.util.base64url.decode(rawHeader).toString("utf-8")
+    );
+  } catch {
+    throw new JWTError("malformed token");
+  }
+
+  const key = await verifyHeader(header);
 }
