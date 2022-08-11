0befcd699d0494c49da42181981bd6e694a5bd7f
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-08-11 16:36:20 +0800

    build(jest): use .spec or .test extension for actual tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
-  testRegex: "(/tests/.*|\\.(test|spec))\\.(ts|tsx)$",
+  testRegex: "/tests/.*\\.(test|spec)\\.(ts|tsx)$",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
