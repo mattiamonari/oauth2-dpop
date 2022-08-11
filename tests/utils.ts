b109e62f164cff63b497032594d08e5478be556f
Zolboobayar Gantumur <zolboobayar@metainc.mn>
2022-08-11 18:04:08 +0800

    feat(test-utils): add signToken method

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,4 +1,4 @@
-import jose from "node-jose";
+import jose, { JWK, JWS } from "node-jose";
 
 export function createToken([header, payload, signature]: [
   header: Record<string, unknown>,
@@ -10,4 +10,28 @@ export function createToken([header, payload, signature]: [
     jose.util.base64url.encode(JSON.stringify(payload)),
     jose.util.base64url.encode(signature),
   ].join(".");
+}
+
+export async function signToken(
+  [header, payload]: [
+    header: Record<string, unknown>,
+    payload: Record<string, unknown>
+  ],
+  key: JWK.Key
+): Promise<string> {
+  const result = (await JWS.createSign(
+    {
+      alg: "ES256",
+      format: "flattened",
+      fields: { kid: undefined, ...header },
+    },
+    key
+  )
+    .update(JSON.stringify(payload))
+    .final()) as unknown as {
+    signature: string;
+    protected: string;
+    payload: string;
+  };
+  return [result.protected, result.payload, result.signature].join(".");
 }
